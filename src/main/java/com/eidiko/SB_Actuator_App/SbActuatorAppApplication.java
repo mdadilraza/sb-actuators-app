163481cf2e15715c3dfb58d36f94801bc5c854f1
mdadilraza <mdadilraza15@gmail.com>
2024-07-10 18:39:21 +0530

    first commit

---
+++ src/main/java/com/eidiko/SB_Actuator_App/SbActuatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.eidiko.SB_Actuator_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbActuatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbActuatorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eidiko/SB_Actuator_App/SbActuatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eidiko.SB_Actuator_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbActuatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
